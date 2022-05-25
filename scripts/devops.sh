70dd29cccbd70e225011140970ce5d9b5b3f7daf
Grell Gragham <vrt.epm@protonmail.com>
2022-05-25 16:23:39 +0300

    Add: vscodium installation with extensions

--- scripts/devops.sh
+++ scripts/devops.sh
@@ -58,6 +58,19 @@ installKubectl() {
     sudo -u "$USERNAME" chmod +x "$BIN_PATH/$kubectlFileName"
 }
 
+installVscodium() {
+    rpmkeys --import https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/-/raw/master/pub.gpg
+    printf "[gitlab.com_paulcarroty_vscodium_repo]\nname=download.vscodium.com\nbaseurl=https://download.vscodium.com/rpms/\nenabled=1\ngpgcheck=1\nrepo_gpgcheck=1\ngpgkey=https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/-/raw/master/pub.gpg\nmetadata_expire=1h" |
+        tee -a /etc/yum.repos.d/vscodium.repo
+    sudo dnf install -y codium
+
+    file=$(cat $PKG_DIR/vscode_extensions.pkgs)
+    IFS=$'\n'
+    for extension in $file; do
+        sudo -u "$USERNAME" codium --install-extension "$extension"
+    done
+}
+
 cleanTmp() {
     bash "$SCRIPT_PATH/clean_tmp.sh"
 }
@@ -68,4 +81,5 @@ installDocker
 installTerraform
 installMinikube
 installKubectl
+installVscodium
 cleanTmp
