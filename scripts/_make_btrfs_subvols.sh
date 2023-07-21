ff0a95a958b2cc85e47f9fd713a4e11a791bd68a
Grell Gragham <vrt.epm@protonmail.com>
2023-07-21 03:54:20 +0300

    refactor: [script] little bit cleanup _make_btrfs_bubvols script

--- scripts/_make_btrfs_subvols.sh
+++ scripts/_make_btrfs_subvols.sh
@@ -8,12 +8,13 @@ DIR_NAME="$2"
 
 # set -euo pipefail
 
+# Backup dir.
 if [[ -d $CURRENT_DIR ]]; then
-    currentTimestamp=""
     currentTimestamp="$(date +'%d_%m_%Y_%H_%M_%S')"
     mv "$CURRENT_DIR" "$BACKUP_PATH/${DIR_NAME}_$currentTimestamp"
 fi
 
+# Create BTRFS subvol.
 if btrfs subvolume create "$CURRENT_DIR"; then
     echo "Nested BTRFS subvolume for $CURRENT_DIR has been created"
 else
